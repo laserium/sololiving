e6be29e4fc6c5a0261fd51a4138b23aae4326f73
feat3719 <rksk8888@naver.com>
2024-06-25 07:07:08 +0900

    TEST : AWS CodeDeploy CI/CD test

--- src/test/java/com/sololiving/home/HomeApplicationTests.java
+++ src/test/java/com/sololiving/home/HomeApplicationTests.java
@@ -1,9 +1,14 @@
 package com.sololiving.home;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class HomeApplicationTests {
 
 	@Test
