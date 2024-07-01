9b476cf1d854e20be38d15016594c7056485596f
feat3719 <rksk8888@naver.com>
2024-07-01 23:09:07 +0900

    TEST : AWS CodeDeploy CI/CD test

--- src/main/java/com/sololiving/home/HostserverApplication.java
+++ src/main/java/com/sololiving/home/HostserverApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-// cicd
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan("com.sololiving.home.mapper")
 public class HostserverApplication {
