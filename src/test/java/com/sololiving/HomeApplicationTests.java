d5da32cc7e2c2adfa4051afbee424486776ed628
feat3719 <rksk8888@naver.com>
2024-08-21 00:05:09 +0900

    [INFRA] AWS CodeDeploy CI/CD 테스트 - 9

--- src/test/java/com/sololiving/HomeApplicationTests.java
+++ src/test/java/com/sololiving/HomeApplicationTests.java
@@ -3,7 +3,7 @@ package com.sololiving;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = HomeApplicationTests.class)
+@SpringBootTest
 class HomeApplicationTests {
 
     @Test
