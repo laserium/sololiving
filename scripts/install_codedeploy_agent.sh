267d755c1df6eb9ba5ec312dde673f1cc67cd382
feat3719 <rksk8888@naver.com>
2024-08-23 23:22:33 +0900

    [INFRA] AWS CodeDeploy CI/CD 테스트 - 43 : start_server.sh 수정 : springboot 애플리케이션 실행시 오류 로그 출력

--- scripts/install_codedeploy_agent.sh
+++ scripts/install_codedeploy_agent.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Check if CodeDeploy Agent is installed
-if systemctl status codedeploy-agent > /dev/null 2>&1; then
+sudo apt-get update
+
+if sudo systemctl status codedeploy-agent > /dev/null 2>&1; then
     echo "CodeDeploy Agent is already installed."
 else
     echo "Installing CodeDeploy Agent..."
-    sudo apt-get update
     sudo apt-get install -y ruby wget
     cd /home/ubuntu
     wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
