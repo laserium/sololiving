02817ac8627d7b64f02d1d8f062a3bc08ad6db28
feat3719 <rksk8888@naver.com>
2024-08-24 00:53:32 +0900

    [INFRA] AWS CodeDeploy CI/CD 테스트 - 46 : install_codedeploy_agent.sh 수정

--- scripts/install_codedeploy_agent.sh
+++ scripts/install_codedeploy_agent.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# Check if CodeDeploy Agent is installed
-if dpkg -l | grep codedeploy-agent > /dev/null 2>&1; then
+sudo apt-get update
+
+if [-f "$/home/ubuntu/install"]; then
     echo "CodeDeploy Agent is already installed."
 else
-    echo "Installing CodeDeploy Agent..."
-    sudo apt-get update
     sudo apt-get install -y ruby wget
-
     cd /home/ubuntu
     wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
     chmod +x ./install
     sudo ./install auto
-
     sudo systemctl start codedeploy-agent
-fi
-
-# Confirm that the CodeDeploy agent is running
-if sudo systemctl is-active --quiet codedeploy-agent; then
-    echo "CodeDeploy Agent is running."
-else
-    echo "CodeDeploy Agent failed to start. Please check the logs."
-    exit 1
 fi
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 echo "Starting application..."
-
-# 로그 파일의 경로
-LOG_FILE="/home/ubuntu/sololiving.jar/sololiving.log"
-
-# 로그 파일을 작성할 수 있도록 디렉터리 권한 수정
-sudo chmod 755 /home/ubuntu/sololiving.jar
-
-# 애플리케이션 시작 및 로그 출력
-nohup java -jar /home/ubuntu/sololiving.jar/sololiving-0.0.1-SNAPSHOT.jar > $LOG_FILE 2>&1 &
-
+nohup java -jar /home/ubuntu/sololiving.jar/sololiving-0.0.1-SNAPSHOT.jar 2>&1 &
 echo "Waiting for the application to start..."
 sleep 60
-
-# 애플리케이션이 성공적으로 시작되었는지 확인
-if ! pgrep -f "sololiving-0.0.1-SNAPSHOT.jar"; then
-    echo "Application failed to start within 60 seconds."
-    echo "Check the log at $LOG_FILE for more information."
-    exit 1
-fi
-
-echo "Application started successfully."
