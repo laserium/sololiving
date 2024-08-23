833872d324f1a06b6cfd26a9cf94f8a9dd235322
feat3719 <rksk8888@naver.com>
2024-08-23 23:36:48 +0900

    [INFRA] AWS CodeDeploy CI/CD 테스트 - 44 : install_codedeploy_agent.sh 수정

--- scripts/install_codedeploy_agent.sh
+++ scripts/install_codedeploy_agent.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-# Check if CodeDeploy Agent is installed
-sudo apt-get update
 
-if sudo systemctl status codedeploy-agent > /dev/null 2>&1; then
+# Check if CodeDeploy Agent is installed
+if systemctl status codedeploy-agent > /dev/null 2>&1; then
     echo "CodeDeploy Agent is already installed."
 else
     echo "Installing CodeDeploy Agent..."
+    sudo apt-get update
     sudo apt-get install -y ruby wget
+
     cd /home/ubuntu
     wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
     chmod +x ./install
     sudo ./install auto
-    sudo service codedeploy-agent start
+
+    sudo systemctl start codedeploy-agent
 fi
 
-sudo service codedeploy-agent status
+# Confirm that the CodeDeploy agent is running
+if systemctl status codedeploy-agent > /dev/null 2>&1; then
+    echo "CodeDeploy Agent is running."
+else
+    echo "CodeDeploy Agent failed to start. Please check the logs."
+    exit 1
+fi
