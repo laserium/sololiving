14a5e9ee89fbd915045fb2799e362c501f392593
feat3719 <rksk8888@naver.com>
2024-08-24 04:50:31 +0900

    [INFRA] AWS CodeDeploy CI/CD 테스트 - 54

--- src/main/java/com/sololiving/SoloLivingApiApplication.java
+++ src/main/java/com/sololiving/SoloLivingApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-// CI/CD TEST 53
+// CI/CD TEST 54
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableAspectJAutoProxy
 public class SoloLivingApiApplication {
