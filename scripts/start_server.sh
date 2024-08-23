41f590f61b734e4be4912499d9358da6f8837e00
feat3719 <rksk8888@naver.com>
2024-08-23 21:50:46 +0900

    [INFRA] AWS CodeDeploy CI/CD 테스트 - 41 : start_server.sh 수정 : springboot 애플리케이션 실행시 오류 로그 출력

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 echo "Starting application..."
-nohup java -jar /home/ubuntu/sololiving.jar/sololiving-0.0.1-SNAPSHOT.jar > /home/ubuntu/sololiving.jar/sololiving.log 2>&1 &
+
+# 로그 파일의 경로
+LOG_FILE="/home/ubuntu/sololiving.jar/sololiving.log"
+
+# 로그 파일을 작성할 수 있도록 디렉터리 권한 수정
+sudo chmod 755 /home/ubuntu/sololiving.jar
+
+# 애플리케이션 시작 및 로그 출력
+nohup java -jar /home/ubuntu/sololiving.jar/sololiving-0.0.1-SNAPSHOT.jar > $LOG_FILE 2>&1 &
 
 echo "Waiting for the application to start..."
 sleep 60
 
-# Check if the application started successfully
+# 애플리케이션이 성공적으로 시작되었는지 확인
 if ! pgrep -f "sololiving-0.0.1-SNAPSHOT.jar"; then
     echo "Application failed to start within 60 seconds."
-    echo "Check the log at /home/ubuntu/sololiving.jar/sololiving.log for more information."
+    echo "Check the log at $LOG_FILE for more information."
     exit 1
 fi
 
