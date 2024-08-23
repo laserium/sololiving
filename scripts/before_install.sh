f1d6e3e3d6addc2a9a542fab3ac6314c8212d4a2
feat3719 <rksk8888@naver.com>
2024-08-23 20:31:38 +0900

    [INFRA] AWS CodeDeploy CI/CD 테스트 - 37 : before_isntall.sh 파일 수정(JAVA설치)

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "Stopping any existing Java applications..."
 pkill -f 'java -jar /home/ubuntu/sololiving.jar' || true
+
+# Java 설치
+if ! java -version &>/dev/null; then
+    echo "Java is not installed. Installing OpenJDK 17..."
+    sudo apt-get update
+    sudo apt-get install -y openjdk-17-jdk
+else
+    echo "Java is already installed."
+fi
