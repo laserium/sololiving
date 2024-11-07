6cfbebc7898013269e8e3ff9bceebbde614c78b1
feat3719 <rksk8888@naver.com>
2024-11-07 19:43:39 +0900

    [INFRA] CI/CD 테스트 - 64

--- src/main/java/com/sololiving/SoloLivingApiApplication.java
+++ src/main/java/com/sololiving/SoloLivingApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-// CI/CD TEST 63
+// CI/CD TEST 64
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableAspectJAutoProxy
 public class SoloLivingApiApplication {
