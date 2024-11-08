0c06fd32e6d9cb5836cdc419a281daf9dd1051f3
feat3719 <rksk8888@naver.com>
2024-11-08 14:07:18 +0900

    [FIX] Redis 오류 해결 및 재 배포

--- src/main/java/com/sololiving/SoloLivingApiApplication.java
+++ src/main/java/com/sololiving/SoloLivingApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-// CI/CD TEST 75
+// CI/CD TEST 76
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableAspectJAutoProxy
 public class SoloLivingApiApplication {
