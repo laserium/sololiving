97a9604ea6b2e8ed22ade5ef20fd3251340046fa
feat3719 <rksk8888@naver.com>
2024-11-17 07:26:30 +0900

    [FIX] 오류 해결

--- src/main/java/com/sololiving/global/config/WebConfig.java
+++ src/main/java/com/sololiving/global/config/WebConfig.java
@@ -16,8 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:3000")
-                .allowedOrigins("https://holoseogi.com")
+                .allowedOrigins("https://localhost:3000, https://holoseogi.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
